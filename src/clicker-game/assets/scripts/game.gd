1ad45313b0889ef1af1f003a26df554609350444
WaterLemur <38868316+WaterLemur@users.noreply.github.com>
2024-09-04 17:31:01 -0500

    bugless click-counter
    ;D

--- src/clicker-game/assets/scripts/game.gd
+++ src/clicker-game/assets/scripts/game.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var clicks = 0
+var clicks: int = 0
 
 var upgrade_1_price = 10
 var upgrade_2_price = 20
@@ -12,15 +12,15 @@ var upgrade_1_level = 0
 var upgrade_2_level = 0
 
 # REFERENCES
-@onready var l_clicks = $"HUD/Pulses/Click Number"
-@onready var l_level_1 = $"HUD/Upgrade 1"
-@onready var l_level_2 = $"HUD/Upgrade 2"
-@onready var l_price_1 = $"HUD/Upgrade 1/Upgrade 1 price"
-@onready var l_price_2 = $"HUD/Upgrade 2/Upgrade 2 price"
+@onready var l_resource = $"HUD/Resource/Click Number"
+@onready var l_level_1 = $"HUD/Upgrades/Upgrade 1"
+@onready var l_level_2 = $"HUD/Upgrades/Upgrade 2"
+@onready var l_price_1 = $"HUD/Upgrades/Upgrade 1/Upgrade 1 price"
+@onready var l_price_2 = $"HUD/Upgrades/Upgrade 2/Upgrade 2 price"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,25 +28,25 @@ func _process(delta: float) -> void:
 	pass
 
 
-func clicked() -> void:
+func _click_button() -> void:
 	clicks = clicks + 1
-	l_clicks.text = str(clicks)
+	l_resource.text = str(clicks)
+	print(clicks)
 
-
-func upgrade_1_pressed() -> void:
+func _upgrade_1_button() -> void:
 	if clicks >= upgrade_1_price:
 		clicks -= upgrade_1_price
 		upgrade_1_level += 1
 		upgrade_1_price = upgrade_1_price * upgrade_1_x_price
 		l_level_1.text = str(upgrade_1_level)
 		l_price_1.text = str(upgrade_1_price)
-		l_clicks.text = str(clicks)
+		l_resource.text = str(clicks)
 	
-func upgrade_2_pressed() -> void:
+func _upgrade_2_button() -> void:
 	if clicks >= upgrade_2_price:
 		clicks -= upgrade_2_price
 		upgrade_2_level += 1
 		upgrade_2_price = upgrade_2_price * upgrade_2_x_price
 		l_level_2.text = str(upgrade_2_level)
 		l_price_2.text = str(upgrade_2_price)
-		l_clicks.text = str(clicks)
+		l_resource.text = str(clicks)
--- src/clicker-game/assets/scripts/system.gd
+++ src/clicker-game/assets/scripts/system.gd
@@ -4,41 +4,26 @@ var intro = preload("res://scns/intro.tscn").instantiate()
 var main = preload("res://scns/main.tscn").instantiate()
 var game = preload("res://scns/game.tscn").instantiate()
 
-var timer = 0.0
-var maxTimer = 3.0
-var timerSpeed = 1.0
+var timer = 3.0
 var state = 0
-
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	timer = maxTimer
+	_load_intro()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if timer <= 0:
-		if state > 2:
-			return
-		else: state += 1
-		
-		timer = maxTimer
-		if state == 0:
-			_load_intro()
-		elif state == 1:
+	if state == 0:
+		if timer <= 0: 
+			state = 1
 			_load_main()
-		elif state == 2:
-			_load_game()
-	else: timer -= timerSpeed * delta
+		else:
+			timer -= delta
 
 func _load_intro():
-	get_tree().root.remove_child(game)
 	get_tree().root.add_child(intro)
 	
 func _load_main():
 	get_tree().root.remove_child(intro)
 	get_tree().root.add_child(main)
-
-func _load_game():
-	get_tree().root.remove_child(main)
-	get_tree().root.add_child(game)
