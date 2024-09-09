58bd764e2d22ea515c6b598631432f3e48c11a1c
WaterLemur <38868316+WaterLemur@users.noreply.github.com>
2024-09-09 15:14:46 -0500

    button colors active state
    a

--- src/clicker-game/assets/scripts/game.gd
+++ src/clicker-game/assets/scripts/game.gd
@@ -25,6 +25,18 @@ var upgrade_2_level = 0
 @onready var img_lungs = $Upgrades/Lungs
 @onready var img_rib_cage = $"Upgrades/Rib Cage"
 
+@onready var btn_brain = $"Main buttons/Button Brain"
+@onready var btn_eye = $"Main buttons/Button Eye"
+@onready var btn_lungs = $"Main buttons/Button Lungs"
+@onready var btn_rib_cage = $"Main buttons/Button Rib Cage"
+
+@onready var btn_upgrade_1 = $"Upgrades2/Button Upgrade 1"
+@onready var btn_upgrade_2 = $"Upgrades2/Button Upgrade 2"
+
+var color_btn_off = Color(1,0,0,1)
+var color_btn_on = Color(0,1,0,1)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	l_resource.text = resource_name + ":"
@@ -33,6 +45,13 @@ func _ready() -> void:
 	img_eye.visible = false
 	img_lungs.visible = false
 	img_rib_cage.visible = false
+	
+	btn_brain.set_modulate(color_btn_off)
+	btn_eye.set_modulate(color_btn_off)
+	btn_lungs.set_modulate(color_btn_off)
+	btn_rib_cage.set_modulate(color_btn_off)
+	btn_upgrade_1.set_modulate(color_btn_off)
+	btn_upgrade_2.set_modulate(color_btn_off)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -65,15 +84,13 @@ func _on_click_pressed() -> void:
 
 func _on_button_brain_pressed() -> void:
 	img_brain.visible = true
-
-
+	btn_brain.set_modulate(color_btn_on)
 func _on_button_eye_pressed() -> void:
 	img_eye.visible = true
-
-
+	btn_eye.set_modulate(color_btn_on)
 func _on_button_lungs_pressed() -> void:
 	img_lungs.visible = true
-
-
+	btn_lungs.set_modulate(color_btn_on)
 func _on_button_rib_cage_pressed() -> void:
 	img_rib_cage.visible = true
+	btn_rib_cage.set_modulate(color_btn_on)
